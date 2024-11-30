c943b6d2166e8bb9190ed346a33ca7f37d3340c5
Oluwatosin Ogunfile <tosinogunfile@gmail.com>
2024-11-30 23:02:56 +0100

    Update setup.sh - nav to environment dir

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/ec2-user/environment
+
 # Prompt the user for the User Pool Domain Prefix
 read -p "Please enter the User Pool Domain Prefix (e.g., labbirdapp-####): " user_input
 
